be1cd69d3dd199264446f240e096af1db2b6404a
Chad Queen <chaddolan82@gmail.com>
2014-07-16 22:09:43 -0600

    Reworked bundling for LESS, SASS, TypeScript
    -Removed css based Twitter package
    -Added less based Twitter package
    -Added BundleTransformers and JSEngines for proper bundling of
    preprocessors and supersets
    -Reworked BundleConfig to utilize transformers

--- WealthHealth/App_Start/BundleConfig.cs
+++ WealthHealth/App_Start/BundleConfig.cs
@@ -1,32 +1,73 @@
-﻿using System.Web;
+﻿using BundleTransformer.Core.Builders;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
 using System.Web.Optimization;
 
 namespace WealthHealth
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // general config
+            bundles.UseCdn = false;
+
+            // bundles by type
+            RegisterJs(bundles);
+            RegisterCss(bundles);
+
+            BundleTable.EnableOptimizations = false;
+        }
+
+        private static void RegisterJs(BundleCollection bundles)
+        {
+            var scriptTransformer = new ScriptTransformer();
+            var nullOrderer = new NullOrderer();
+            var nullBuilder = new NullBuilder();
+
+            // jQuery
+            var jqueryBundle = new Bundle("~/bundles/js/jquery")
+                .Include("~/Scripts/jquery-{version}.js");
+            jqueryBundle.Builder = nullBuilder;
+            jqueryBundle.Transforms.Add(scriptTransformer);
+            jqueryBundle.Orderer = nullOrderer;
+            bundles.Add(jqueryBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            var modernizrBundle = new Bundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*");
+            modernizrBundle.Builder = nullBuilder;
+            modernizrBundle.Transforms.Add(scriptTransformer);
+            modernizrBundle.Orderer = nullOrderer;
+            bundles.Add(modernizrBundle);
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            var vendorBundle = new Bundle("~/bundles/js/vendor")
+                .Include(
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js");
+            vendorBundle.Builder = nullBuilder;
+            vendorBundle.Transforms.Add(scriptTransformer);
+            vendorBundle.Orderer = nullOrderer;
+            bundles.Add(vendorBundle);
+        }
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+        private static void RegisterCss(BundleCollection bundles)
+        {
+            var styleTransformer = new StyleTransformer();
+            var cssMinify = new CssMinify();
+            var nullOrderer = new NullOrderer();
+            var nullBuilder = new NullBuilder();
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            var appStyleBundle = new Bundle("~/bundles/css/app")
+                .Include(
+                    "~/Content/less/build.less",
+                    "~/Content/sass/site.scss");
+            appStyleBundle.Builder = nullBuilder;
+            appStyleBundle.Transforms.Add(styleTransformer);
+            appStyleBundle.Transforms.Add(cssMinify);
+            appStyleBundle.Orderer = nullOrderer;
+            bundles.Add(appStyleBundle);
         }
     }
 }
