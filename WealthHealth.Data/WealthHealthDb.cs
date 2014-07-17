0000b9c15f85457f0fabe4552b6e836912779cd1
Chad Queen <chaddolan82@gmail.com>
2014-07-16 22:30:26 -0600

    Enabled database migrations, setup application user
    -Added Model and Data projects
    -Added first model ApplicationUser to reflect the ASP.NET Identity user
    -Added the DbContext inline with the Identity setup for EntityFramework
    -Enabled Entity Framework Code First migrations
    -Added the initial migration for Identity tables

---
+++ WealthHealth.Data/WealthHealthDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using WealthHealth.Model.Core;
+
+namespace WealthHealth.Data
+{
+    public class WealthHealthDb : IdentityDbContext<ApplicationUser>
+    {
+        public WealthHealthDb()
+            : base("WealthHealth", throwIfV1Schema: false)
+        {
+        }
+
+        public static WealthHealthDb Create()
+        {
+            return new WealthHealthDb();
+        }
+    }
+}
---
+++ WealthHealth.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WealthHealth.Model")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WealthHealth.Model")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c4f6558-51eb-48eb-bb20-3354143da14f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
